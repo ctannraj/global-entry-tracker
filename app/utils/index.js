a9647dfd9e820216b3d0bc0452e66db6bf5a8cbe
ctannraj <161249622+ctannraj@users.noreply.github.com>
2024-04-18 22:05:32 -0700

    Update index.js

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,8 +1,22 @@
 const path = require('path');
 
+function updateScheduleInSeconds(configs) {
+  let value = 60;
+  try {
+    let _value = eval(configs.scheduleInSeconds);
+    if (_value > 60) {
+      value = _value;
+    }
+  } catch (err) {
+  }
+  configs.scheduleInSeconds = value;
+  configs.scheduleInMinutes = Math.ceil(value / 60);
+}
+
 function loadConfigs() {
   let configFilePath = path.normalize(`${process.cwd()}/config/config.js`);
   let configs = require(configFilePath);
+  updateScheduleInSeconds(configs);
   return configs;
 }
 
